d60d94ea8bdd54b376f4cafdf5b7a98f1c441ea5
AswinKarthikeyan <aswinbabu99675@gmail.com>
2026-08-08 16:41:03 +0530

    Run QKeys website and invoice system on Render

--- qkeys/urls.py
+++ qkeys/urls.py
@@ -1,27 +1,3 @@
-"""
-URL configuration for qkeys project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('properties.urls')),
-]
-
 from django.contrib import admin
 from django.urls import path, include
 
@@ -31,37 +7,42 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path("admin/", admin.site.urls),
-
-    path("", include("properties.urls")),
-
-]
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("properties.urls")),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
-    from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-
+    # =========================
     # Django Admin
+    # =========================
+
     path(
         'admin/',
         admin.site.urls
     ),
 
-    # Invoice App
+    # =========================
+    # QKeys.in / Properties
+    # =========================
+
+    path(
+        '',
+        include('properties.urls')
+    ),
+
+    # =========================
+    # Invoice System
+    # =========================
+
     path(
         'invoices/',
         include('invoices.urls')
     ),
-
 ]
+
+
+# =========================
+# Media Files - Development
+# =========================
+
+if settings.DEBUG:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
